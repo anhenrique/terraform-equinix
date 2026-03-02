bfc2fef1634d5e6186cfd8683d72626705675e3f
Andre Henrique Guedes da Silva <andre.silva@hsprevent.com.br>
2026-03-02 16:09:41 +0000

    Edit backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,14 @@
 terraform {
   backend "http" {
-    address        = "https://gitlab.com/api/v4/projects/387/terraform/state/terraform-state"
-    lock_address   = "https://gitlab.com/api/v4/projects/387/terraform/state/terraform-state/lock"
-    unlock_address = "https://gitlab.com/api/v4/projects/387/terraform/state/terraform-state/lock"
+    address        = "https://gitlab-hs.hsprevent.com.br/api/v4/projects/387/terraform/state/dev"
+    lock_address   = "https://gitlab-hs.hsprevent.com.br/api/v4/projects/387/terraform/state/dev/lock"
+    unlock_address = "https://gitlab-hs.hsprevent.com.br/api/v4/projects/387/terraform/state/dev/lock"
     lock_method    = "POST"
     unlock_method  = "DELETE"
     retry_wait_min = 5
   }
 }
+
+
 
 
