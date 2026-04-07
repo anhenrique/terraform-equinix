98ae9c21e88c108d6e6e80a0b7d09902fcfc3b2f
2623.andre <andre.silva@hsprevent.com.br>
2026-04-07 20:47:03 -0300

    network fixes

--- infra-vapp/vapp.tf
+++ infra-vapp/vapp.tf
@@ -55,7 +55,8 @@ resource "vcd_vapp_org_network" "networks" {
   vapp_name        = vcd_vapp.my_vapp.name
   org_network_name = var.vapp_network
 
-  reboot_vapp_on_removal = true
-
-  depends_on = [vcd_vapp.my_vapp]
+  # Esta configuração impede que o Terraform destrua o recurso
+  lifecycle {
+    prevent_destroy = true
+  }
 }
