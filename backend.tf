08e154c7a7bc7bc36757dc5e10bb17353ba3e21b
2623.andre <andre.silva@hsprevent.com.br>
2026-04-08 11:39:23 -0300

    state prd fix

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "http" {
-    address        = "https://gitlab-hs.hsprevent.com.br/api/v4/projects/387/terraform/state/dev"
-    lock_address   = "https://gitlab-hs.hsprevent.com.br/api/v4/projects/387/terraform/state/dev/lock"
-    unlock_address = "https://gitlab-hs.hsprevent.com.br/api/v4/projects/387/terraform/state/dev/lock"
+    address        = "https://gitlab-hs.hsprevent.com.br/api/v4/projects/387/terraform/state/prd"
+    lock_address   = "https://gitlab-hs.hsprevent.com.br/api/v4/projects/387/terraform/state/prd/lock"
+    unlock_address = "https://gitlab-hs.hsprevent.com.br/api/v4/projects/387/terraform/state/prd/lock"
     lock_method    = "POST"
     unlock_method  = "DELETE"
     retry_wait_min = 5
