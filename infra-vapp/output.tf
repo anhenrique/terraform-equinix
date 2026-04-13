d94767363f2fd2d7c40ede7235633db8c8dfa98c
2623.andre <andre.silva@hsprevent.com.br>
2026-04-13 10:04:29 -0300

    fix new vm variable

--- infra-vapp/output.tf
+++ infra-vapp/output.tf
@@ -5,23 +5,23 @@ output "vapp_name" {
 
 output "vm_names" {
   description = "Nomes das VMs criadas"
-  value       = [for vm in vcd_vapp_vm.vms : vm.name]
+  value       = [for vm in vcd_vm.vms : vm.name]
 }
 
 output "vm_ids" {
   description = "IDs das VMs criadas"
-  value       = [for vm in vcd_vapp_vm.vms : vm.id]
+  value       = [for vm in vcd_vm.vms : vm.id]
 }
 
 output "vapp_vm_ips" {
   description = "Lista de endereços IP das instâncias na Equinix"
   # Observe o  logo após a palavra network
-  value = [for vm in vcd_vapp_vm.vms : vm.network[0].ip]
+  value = [for vm in vcd_vm.vms : vm.network[0].ip]
 }
 
 output "vapp_vm_ips_concatenados" {
   description = "Lista de VMs e seus respectivos IPs na Equinix"
   # Observe o uso do índice  que corrigimos anteriormente
-  value = [for vm in vcd_vapp_vm.vms : "${vm.name}: ${vm.network[0].ip}"]
+  value = [for vm in vcd_vm.vms : "${vm.name}: ${vm.network[0].ip}"]
 }
 
