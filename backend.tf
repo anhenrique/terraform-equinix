f870c715b689ebef57c0921f8f1126acfb0edd48
2623.andre <andre.silva@hsprevent.com.br>
2026-04-16 16:27:33 -0300

    fix bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-starter-bucket-b8uzs "
+    bucket = "terraform-starter-bucket-b8uzs"
     key    = "projeto/terraform.tfstate" 
     region = "sa-east-1"                 
     
